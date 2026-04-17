9e2024987f4a306e83480213526694116b4cf2b5
Valentin Ivaşcu <oriceon@gmail.com>
2026-04-17 13:07:47 +0300

    Add jsonFileName parameter to constructor

--- src/LaravelGmailClass.php
+++ src/LaravelGmailClass.php
@@ -8,68 +8,68 @@ use Illuminate\Support\Facades\Redirect;
 
 class LaravelGmailClass extends GmailConnection
 {
-	public function __construct($config, $userId = null)
-	{
-		if (class_basename($config) === 'Application') {
-			$config = $config['config'];
-		}
+    public function __construct($config, $userId = null, $jsonFileName = null)
+    {
+        if (class_basename($config) === 'Application') {
+            $config = $config['config'];
+        }
 
-		parent::__construct($config, $userId);
-	}
+        parent::__construct($config, $userId, $jsonFileName);
+    }
 
-	/**
-	 * @return Message
-	 * @throws AuthException
-	 */
-	public function message()
-	{
-		if (!$this->getToken()) {
-			throw new AuthException('No credentials found.');
-		}
+    /**
+     * @throws AuthException
+     * @return Message
+     */
+    public function message()
+    {
+        if ( ! $this->getToken()) {
+            throw new AuthException('No credentials found.');
+        }
 
-		return new Message($this);
-	}
+        return new Message($this);
+    }
 
-	/**
-	 * Returns the Gmail user email
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function user()
-	{
-		return $this->config('email');
-	}
+    /**
+     * Returns the Gmail user email
+     *
+     * @return \Google_Service_Gmail_Profile
+     */
+    public function user()
+    {
+        return $this->config('email');
+    }
 
-	/**
-	 * Updates / sets the current userId for the service
-	 *
-	 * @return \Google_Service_Gmail_Profile
-	 */
-	public function setUserId($userId)
-	{
-		$this->userId = $userId;
-		return $this;
-	}
+    /**
+     * Updates / sets the current userId for the service
+     *
+     * @return \Google_Service_Gmail_Profile
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
 
-	public function redirect()
-	{
-		return Redirect::to($this->getAuthUrl());
-	}
+        return $this;
+    }
 
-	/**
-	 * Gets the URL to authorize the user
-	 *
-	 * @return string
-	 */
-	public function getAuthUrl()
-	{
-		return $this->createAuthUrl();
-	}
+    public function redirect()
+    {
+        return Redirect::to($this->getAuthUrl());
+    }
 
-	public function logout()
-	{
-		$this->revokeToken();
-		$this->deleteAccessToken();
-	}
+    /**
+     * Gets the URL to authorize the user
+     *
+     * @return string
+     */
+    public function getAuthUrl()
+    {
+        return $this->createAuthUrl();
+    }
 
+    public function logout()
+    {
+        $this->revokeToken();
+        $this->deleteAccessToken();
+    }
 }
